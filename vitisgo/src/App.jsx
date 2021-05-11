2491d55ae831ffde7cfdbfe284f14514478805b3
DThomas51 <thomas.dufour974@gmail.com>
2021-05-11 17:08:44 +0200

    create store button

--- vitisgo/src/App.jsx
+++ vitisgo/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import './App.css';
 import Play from './components/Play'
-import Store from './components/Store'
+
 function App() {
   return (
     <div className="App">
       <Play/>
-      <Store/>
     </div>
   );
 }
