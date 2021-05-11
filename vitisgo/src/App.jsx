bbde41fc8613a299524da0fdce92b44d93fe23ee
DThomas51 <thomas.dufour974@gmail.com>
2021-05-11 13:55:09 +0200

    added components

---
+++ vitisgo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+import Play from './components/Play'
+function App() {
+  return (
+    <div className="App">
+      <Play/>
+    </div>
+  );
+}
+
+export default App;
