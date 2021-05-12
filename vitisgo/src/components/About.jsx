89982287830fbb72729234e8c6896871131b2c93
denis-webpro <denis.webpro@gmail.com>
2021-05-12 10:20:22 +0200

    Setting up the Css on the About.jsx page

--- vitisgo/src/components/About.jsx
+++ vitisgo/src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Logo from '../asset/logo.png';
 import './List.css'
 
 const About = () => {
@@ -10,62 +11,75 @@ const About = () => {
             <br />
             <h2>Quelques mots en rapport avec ces pages</h2>
             <ul>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, sequi?
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum laborum ab veritatis fugiat ducimus, labore alias ipsam, quasi voluptates qui, laudantium dolorum. Debitis consequuntur, error a hic earum sint ea doloribus nam minus porro repellendus delectus dolor. Dolor eaque, sint iure enim recusandae deleniti est odit nesciunt! Sequi consequatur nam iusto quidem ipsam. Impedit accusantium quisquam explicabo facere illum dolorum esse nobis corrupti consectetur culpa nihil, adipisci deleniti qui. Molestias, dolorum!
                 </li>
             </ul>
+            <div className="logo_about">
+            <img className="logo_size" src={Logo} alt="logo" />
+            </div>
+
             <h1>Avantages</h1>
             <br />
             <h2>Quelques mots en rapport avec les avantages</h2>
             <ul>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, sequi?
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum laborum ab veritatis fugiat ducimus, labore alias ipsam, quasi voluptates qui, laudantium dolorum. Debitis consequuntur, error a hic earum sint ea doloribus nam minus porro repellendus delectus dolor. Dolor eaque, sint iure enim recusandae deleniti est odit nesciunt! Sequi consequatur nam iusto quidem ipsam. Impedit accusantium quisquam explicabo facere illum dolorum esse nobis corrupti consectetur culpa nihil, adipisci deleniti qui. Molestias, dolorum!
                 </li>
             </ul>
+            <div className="logo_about">
+            <img className="logo_size" src={Logo} alt="logo" />
+            </div>
             <h1>Comment ?</h1>
             <br />
             <h2>Quelques mots en rapport avec le comment ?</h2>
             <ul>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, sequi?
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum laborum ab veritatis fugiat ducimus, labore alias ipsam, quasi voluptates qui, laudantium dolorum. Debitis consequuntur, error a hic earum sint ea doloribus nam minus porro repellendus delectus dolor. Dolor eaque, sint iure enim recusandae deleniti est odit nesciunt! Sequi consequatur nam iusto quidem ipsam. Impedit accusantium quisquam explicabo facere illum dolorum esse nobis corrupti consectetur culpa nihil, adipisci deleniti qui. Molestias, dolorum!
                 </li>
             </ul>
+            <div className="logo_about">
+            <img className="logo_size" src={Logo} alt="logo" />
+            </div>
             <h1>Pourquoi ?</h1>
             <br />
             <h2>Quelques mots en rapport avec le pourquoi ?</h2>
             <ul>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, sequi?
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                 </li>
-                <li>
+                <li className="li_about">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum repellendus mollitia vitae, perspiciatis odit nihil deleniti fugit quasi aliquid molestias quidem, in dicta omnis, nam itaque voluptates eveniet optio porro exercitationem impedit ab earum temporibus. Omnis, quis.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum laborum ab veritatis fugiat ducimus, labore alias ipsam, quasi voluptates qui, laudantium dolorum. Debitis consequuntur, error a hic earum sint ea doloribus nam minus porro repellendus delectus dolor. Dolor eaque, sint iure enim recusandae deleniti est odit nesciunt! Sequi consequatur nam iusto quidem ipsam. Impedit accusantium quisquam explicabo facere illum dolorum esse nobis corrupti consectetur culpa nihil, adipisci deleniti qui. Molestias, dolorum!
                 </li>
             </ul>
+            <div className="logo_about">
+            <img className="logo_size" src={Logo} alt="logo" />
+            </div>
         </div>             
     );
 };
